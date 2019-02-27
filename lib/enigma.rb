d18093040b869df3db368913549b941d98679aea
Paul Schlattmann <paul.h.schlattmann@gmail.com>
2019-02-27 00:42:14 -0700

    Update enigma.rb

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -37,28 +37,4 @@ end
 
 
 
-# class Enigma
-# include Encrypt
-#
-#
-#   attr_reader :date,
-#               :randomkey,
-#               :alphabet
-#
-#
-#
-#   def initialize
-#     @message = "Hello World"
-#     @time = Time.new
-#     @date = @time.strftime("%d%m%y")
-#     @randomkey = randomkey
-#     @alphabet = ("a".."z").to_a << " "
-#   end
-#
-  # def encrypt(message = @message.downcase, key = @randomkey, date = @date)
-  #     {
-  #     encryption: encrypt_message,
-  #     key: key,
-  #     date: date
-  #     }
-  # end
+
